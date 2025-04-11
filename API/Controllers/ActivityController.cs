b45d7fbb73a4fc1b593005d2555e7ea2ddc084ca
shreyaskhamkar <shreyaskhamkar96@gmail.com>
2025-04-11 20:04:28 +0530

    feat: create other CRUD endpoint server side

--- API/Controllers/ActivityController.cs
+++ API/Controllers/ActivityController.cs
@@ -1,4 +1,8 @@
+using Application.Activities;
+using Application.Activities.Command;
+using Application.Activities.Queries;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -6,22 +10,42 @@ using Persistence;
 namespace API.Controllers;
 
 [Route("api/activities")]
-public class ActivityController(AppDbContext context) : BaseApiController
+public class ActivityController() : BaseApiController
 {
 
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await context.Activities.ToListAsync();
+
+        return await Mediator.Send(new GetActivityList.Query());
+
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        var activity = await context.Activities.FindAsync(id);
+        var activity = await Mediator.Send(new GetActivityDetails.Query { Id = id });
         if (activity == null) return NotFound();
         return activity;
     }
 
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity([FromBody] Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command { Activity = activity });
+    }
 
+    [HttpPut]
+    public async Task<ActionResult> EditActivity(Activity activity)
+    {
+        await Mediator.Send(new EditActivity.Command { Activity = activity });
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteActivity(string id)
+    {
+        await Mediator.Send(new DeleteActivity.Command { Id = id });
+        return Ok();
+    }
 }
